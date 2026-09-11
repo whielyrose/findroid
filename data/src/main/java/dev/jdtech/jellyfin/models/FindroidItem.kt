acc4102c1d2cb81e3f3f67f2a19885495e329a8e
whiely <chrisrose88@proton.me>
2026-09-11 14:12:27 +1000

    Wire AUDIO_BOOK into item dispatcher

--- data/src/main/java/dev/jdtech/jellyfin/models/FindroidItem.kt
+++ data/src/main/java/dev/jdtech/jellyfin/models/FindroidItem.kt
@@ -34,6 +34,7 @@ suspend fun BaseItemDto.toFindroidItem(
         BaseItemKind.SERIES -> toFindroidShow(jellyfinRepository)
         BaseItemKind.BOX_SET -> toFindroidBoxSet(jellyfinRepository)
         BaseItemKind.FOLDER -> toFindroidFolder(jellyfinRepository)
+        BaseItemKind.AUDIO_BOOK -> toFindroidAudiobook(jellyfinRepository, serverDatabase)
         else -> null
     }
 }
